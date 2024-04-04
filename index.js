918d5da84a2d126d9c6ef96f72141a2e054688e8
manasjhamj <jhamanas939@gmail.com>
2024-04-04 23:01:27 +0530

    add sidenav

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 /* Set the width of the sidebar to 250px (show it) */
 function openNav() {
-    document.getElementById("mySidepanel").style.width = "100%";
+    document.getElementById("mySidepanel").style.height = "24%";
 }
 
 /* Set the width of the sidebar to 0 (hide it) */
 function closeNav() {
-    document.getElementById("mySidepanel").style.width = "0";
+    document.getElementById("mySidepanel").style.height = "0";
 }
