3ffed3cc330659382bd847ace71082ea45bd8182
manasjhamj <jhamanas939@gmail.com>
2024-04-09 18:58:06 +0530

    add swiper.js

--- index.js
+++ index.js
@@ -7,3 +7,26 @@ function openNav() {
 function closeNav() {
     document.getElementById("mySidepanel").style.height = "0";
 }
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+        type: "progressbar",
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+});
